dcf3fdd777cc71bdeddcfa11e2ea78cfbbf02972
sagnolo <rladnjs401@gmail.com>
2019-01-10 17:05:15 +0900

    Instance Modified

--- src/Instance_Example/AppleBuyer.java
+++ src/Instance_Example/AppleBuyer.java
@@ -2,16 +2,16 @@ package Instance_Example;
 
 public class AppleBuyer {
 	
-	int myMoney = 10000;
-	int numOfApple = 0;
+	int myMoney = 10000; //보유한 금액
+	int numOfApple = 0; // 소유한 사과 수
 	
-	public void buyApple(AppleSeller seller, int money)
+	public void buyApple(AppleSeller seller, int money) //사과를 구매하는 행위
 	{
 		numOfApple+= seller.saleApple(money);
 		myMoney-=money;
 	}
 	
-	public void showBuy(){
+	public void showBuy(){ //현재 상태 보여주기
 		System.out.println("사과 개수 : " + numOfApple);
 		System.out.println("남은 잔액 : " + myMoney);
 	}
--- src/Instance_Example/AppleSeller.java
+++ src/Instance_Example/AppleSeller.java
@@ -2,9 +2,9 @@ package Instance_Example;
 
 public class AppleSeller {
 
-	int numOfApple = 20;
-	int myMoney = 0;
-	final int APPLE_PRICE=100;
+	int numOfApple = 20; //보유한 사과 수
+	int myMoney = 0; //소유 금액
+	final int APPLE_PRICE=100; //사과 가격
 	
 	public int saleApple(int money){
 		int num = money/APPLE_PRICE;
